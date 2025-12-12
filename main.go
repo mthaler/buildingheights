8d6591364b29f9a94a49a080b40eaac9fce4f2bd
Michael Thaler <michael.thaler2@gmail.com>
2025-12-12 11:28:15 +0100

    Updated main.go.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	fName := "summits.csv"
+	fName := "buildings.csv"
 	file, err := os.Create(fName)
 	if err != nil {
 		log.Fatalf("Cannot create file %q: %s\n", fName, err)
@@ -26,4 +26,21 @@ func main() {
  	// Define the URL you want to scrape
  	url := "https://de.wikipedia.org/wiki/Liste_der_h%C3%B6chsten_Bauwerke_in_M%C3%BCnchen"
 
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Scraping:", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+       	fmt.Println("Status:", r.StatusCode)
+   	})
+
+	c.OnError(func(r *colly.Response, err error) {
+    	fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+   	})
+
+	// Visit the URL and start scraping
+	err = c.Visit(url)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
