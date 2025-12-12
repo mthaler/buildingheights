43a47035234015c5d0b671d1a57279aea7d183e0
Michael Thaler <michael.thaler2@gmail.com>
2025-12-12 12:25:40 +0100

    Updated building.go and main.go.

--- building.go
+++ building.go
@@ -1,11 +1,24 @@
 package main
 
 type Building struct {
-	Name        		string
-	Type				string
-	YearOfConstruction 	string
-	Height 				string
-	Latitude			string
-	Longitude			string
-	Remark				string
+	Name               string
+	Type               string
+	YearOfConstruction string
+	Height             string
+	Latitude           string
+	Longitude          string
+	Remark             string
+}
+
+func (b Building) toSlice() []string {
+	var result []string
+	result = make([]string, 0)
+	result = append(result, b.Name)
+	result = append(result, b.Type)
+	result = append(result, b.YearOfConstruction)
+	result = append(result, b.Height)
+	result = append(result, b.Latitude)
+	result = append(result, b.Longitude)
+	result = append(result, b.Remark)
+	return result
 }
--- main.go
+++ main.go
@@ -25,69 +25,54 @@ func main() {
 
 	c := colly.NewCollector()
 
- 	// Define the URL you want to scrape
- 	url := "https://de.wikipedia.org/wiki/Liste_der_h%C3%B6chsten_Bauwerke_in_M%C3%BCnchen"
+	// Define the URL you want to scrape
+	url := "https://de.wikipedia.org/wiki/Liste_der_h%C3%B6chsten_Bauwerke_in_M%C3%BCnchen"
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Scraping:", r.URL)
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-       	fmt.Println("Status:", r.StatusCode)
-   	})
+		fmt.Println("Status:", r.StatusCode)
+	})
 
 	c.OnHTML("table", func(e *colly.HTMLElement) {
 		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
-			/*height := el.ChildText("td:nth-child(3)")
-			if (height != "") {
-				height = height[:len(height)-2]
-			}
-			if err != nil {
-				fmt.Printf("Could not convert %s to int")
-			}
-			s := Summit{ Name: el.ChildText("td:nth-child(2)"),
-				 Category: el.ChildText("td:nth-child(4)"),
-				 Height: height,
-				 Country: el.ChildText("td:nth-child(5)"),
-				 Region: el.ChildText("td:nth-child(6)"),
-				 Group: el.ChildText("td:nth-child(7)"),
-				 Information: el.ChildText("td:nth-child(8)"),
-			}
-			w.Write(s.toSlice())*/
-
 			height := el.ChildText("td:nth-child(4)")
-			if (height != "") {
+			if height != "" {
 				height = height[:utf8.RuneCountInString(height)-2]
 				height = strings.Replace(height, ",", ".", -1)
 			}
 			coordinates := el.ChildText("td:nth-child(5)")
 			latitude := ""
 			longitude := ""
-			if (coordinates != "") {
+			if coordinates != "" {
 				coordinatesArray := strings.Split(coordinates, ",")
-				if (len(coordinatesArray) == 2) {
+				if len(coordinatesArray) == 2 {
 					latitude = coordinatesArray[0]
-					latitude = latitude[:len(latitude)-3]
+					latitude = latitude[:utf8.RuneCountInString(latitude)-3]
 					longitude = coordinatesArray[1]
-					longitude = longitude[:len(longitude)-3]
+					longitude = longitude[:utf8.RuneCountInString(longitude)-3]
 				}
 			}
-			b := Building{ 
-				Name: el.ChildText("td:nth-child(1)"),
-				Type: el.ChildText("td:nth-child(2)"),
+			b := Building{
+				Name:               el.ChildText("td:nth-child(1)"),
+				Type:               el.ChildText("td:nth-child(2)"),
 				YearOfConstruction: el.ChildText("td:nth-child(3)"),
-				Height: height,
-				Latitude: latitude,
-				Longitude: longitude,
+				Height:             height,
+				Latitude:           latitude,
+				Longitude:          longitude,
+				Remark:             el.ChildText("td:nth-child(6)"),
 			}
 
-			fmt.Printf("%+v\n", b)
+			w.Write(b.toSlice())
+
 		})
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
-    	fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-   	})
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
 
 	// Visit the URL and start scraping
 	err = c.Visit(url)
