5a85e860e16bd972ac39fa48a6335e7163b052a0
Michael Thaler <michael.thaler2@gmail.com>
2025-12-12 11:21:45 +0100

    Updated building.go.

---
+++ building.go
@@ -0,0 +1,11 @@
+package main
+
+type Summit struct {
+	Name        		string
+	Type				string
+	YearOfConstruction 	string
+	Height 				string
+	Latitude			string
+	Longitude			string
+	Remark				string
+}
